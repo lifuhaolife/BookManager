82b54f5310ac3d257f05b2e25730b7e22c892ea1
lfh2448808186 <2448808186@qq.com>
2020-03-30 22:49:57 +0800

    基本环境搭建

---
+++ book_permission/src/main/java/xust/lfh/book_permission/BookPermissionApplication.java
@@ -0,0 +1,13 @@
+package xust.lfh.book_permission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPermissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookPermissionApplication.class, args);
+    }
+
+}
---
+++ book_permission/src/test/java/xust/lfh/book_permission/BookPermissionApplicationTests.java
@@ -0,0 +1,13 @@
+package xust.lfh.book_permission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPermissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
